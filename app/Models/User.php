9361110c38ded65093cb63d91b9631419257f27d
Ung Sochea <sochea.ung@gmail.com>
2022-08-31 17:18:17 +0700

    fix User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use GoldSpecDigital\LaravelEloquentUUID\Foundation\Auth\User as Authenticatable;
-// use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable,Uuid;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
