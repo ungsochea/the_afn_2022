8277ddc5ae602c5519a01dcdf5c04a2dbe6bd372
“ungsochea” <sochea.ung@gmail.com>
2022-12-28 10:39:10 +0700

    count on dashboard

--- resources/views/admins/dashboards/index.blade.php
+++ resources/views/admins/dashboards/index.blade.php
@@ -2,5 +2,72 @@
 @section('main_title','Dashboard')
 
 @section('content')
+<div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
+    <div>
+      <h4 class="mb-3 mb-md-0">Welcome to Dashboard</h4>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12 col-xl-12 stretch-card">
+      <div class="row flex-grow-1">
+        <div class="col-md-3 grid-margin stretch-card">
+            <div class="card">
+              <div class="card-body">
+                <div class="d-flex justify-content-between align-items-baseline">
+                  <h6 class="card-title">Posts</h6>
+                </div>
+                <div class="row">
+                  <div class="col-6 col-md-12 col-xl-5">
+                    <h3 class="mb-2">{{ App\Models\Post::count() ?? '0' }}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
 
+        <div class="col-md-3 grid-margin stretch-card">
+            <div class="card">
+              <div class="card-body">
+                <div class="d-flex justify-content-between align-items-baseline">
+                  <h6 class="card-title">Videos</h6>
+                </div>
+                <div class="row">
+                  <div class="col-6 col-md-12 col-xl-5">
+                    <h3 class="mb-2">{{ App\Models\Video::count() ?? '0' }}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-3 grid-margin stretch-card">
+            <div class="card">
+              <div class="card-body">
+                <div class="d-flex justify-content-between align-items-baseline">
+                  <h6 class="card-title">Contact Us</h6>
+                </div>
+                <div class="row">
+                  <div class="col-6 col-md-12 col-xl-5">
+                    <h3 class="mb-2">{{ App\Models\ContactUs::count() ?? '0' }}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-3 grid-margin stretch-card">
+            <div class="card">
+              <div class="card-body">
+                <div class="d-flex justify-content-between align-items-baseline">
+                  <h6 class="card-title">User</h6>
+                </div>
+                <div class="row">
+                  <div class="col-6 col-md-12 col-xl-5">
+                    <h3 class="mb-2">{{ App\Models\User::count() ?? '0' }}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+</div>
 @endsection
